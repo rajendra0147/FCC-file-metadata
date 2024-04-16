58a73f233f7512612c5917933473a81a5c20988f
Rajendra <rajendrathakur864@gmail.com>
2024-04-16 12:19:16 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var upload = multer();
 
 var app = express();
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  // console.log(req.file)
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
+  res.json({
+    name,
+    type,
+    size
+  })
+})
 
 
 
